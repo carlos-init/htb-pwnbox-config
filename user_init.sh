477567a26f321cb7f4d85af853c04d5ad39d7cea
carlos-init <61292651+carlos-init@users.noreply.github.com>
2025-09-02 08:17:11 -0700

    Create user_init.sh

---
+++ user_init.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+#This script is executed every time your instance is spawned.
+
+cd 
+mkdir Tools
+cd Tools
+mkdir chisel
+cd chisel
+wget https://github.com/jpillora/chisel/releases/download/v1.10.1/chisel_1.10.1_windows_amd64.gz
+gunzip chisel_1.10.1_windows_amd64.gz
+mv chisel_1.10.1_windows_amd64 chisel.exe
+wget https://github.com/jpillora/chisel/releases/download/v1.10.1/chisel_1.10.1_linux_amd64.gz
+gunzip chisel_1.10.1_linux_amd64.gz
+mv chisel_1.10.1_linux_amd64 chisel
+chmod +x chisel
+cd
+cd Tools
+mkdir peass
+cd peass
+wget https://github.com/peass-ng/PEASS-ng/releases/latest/download/linpeas.sh
+wget https://github.com/carlospolop/PEASS-ng/releases/latest/download/winPEASx64.exe
+wget https://github.com/carlospolop/PEASS-ng/releases/latest/download/winPEASx86.exe
+cd 
+cd Tools
+mkdir PrintSpoofer
+cd PrintSpoofer
+wget https://github.com/itm4n/PrintSpoofer/releases/download/v1.0/PrintSpoofer64.exe
+cd
+cd Tools
+mkdir LaZagne
+cd LaZagne
+wget https://github.com/AlessandroZ/LaZagne/releases/latest/download/LaZagne.exe -O LaZagne.exe
+cd
+cd Tools
+mkdir kerbrute
+cd kerbrute
+go install github.com/ropnop/kerbrute@master
+cp ~/go/bin/kerbrute .
+cd
+cd Tools
+git clone https://github.com/Flangvik/SharpCollection.git
+git clone https://github.com/urbanadventurer/username-anarchy.git
+mkdir pwdump
+cd pwdump
+wget https://download.openwall.net/pub/projects/john/contrib/pwdump/pwdump8-8.2.zip
+cd
+cd Tools
+git clone https://github.com/urbanadventurer/username-anarchy.git
+git clone https://github.com/kmahyyg/mremoteng-decrypt.git
+cd 
+sudo apt install eyewitness ca-certificates gh rsyslog exiftool ntpdate tree -y
+
+#configure tmux
+cd
+mkdir ~/.tmux.conf
+
+echo "unbind r" >> ~/.tmux.conf
+ehco "bind r source-file ~/.tmux.conf" >> ~/.tmux.conf
+echo "" >> ~/.tmux.conf
+echo"set -g prefix C-s" >> ~/.tmux.conf
+echo "" >> ~/.tmux.conf
+echo "# Navigate panes with Alt + arrow keys (no prefix)" >> ~/.tmux.conf
+echo "bind -n M-Left select-pane -L" >> ~/.tmux.conf
+echo "bind -n M-Right select-pane -R" >> ~/.tmux.conf
+echo "bind -n M-Up select-pane -U" >> ~/.tmux.conf
+echo "bind -n M-Down select-pane -D" >> ~/.tmux.conf
+
+
+
+
+# git clone https://github.com/nicocha30/ligolo-ng.git
